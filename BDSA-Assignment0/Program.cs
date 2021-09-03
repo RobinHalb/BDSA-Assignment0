6bdebe211f36523fae06f861f7385566d110b7c9
RobinHalb <78663194+RobinHalb@users.noreply.github.com>
2021-09-03 14:50:48 +0200

    Created project base (#1)

---
+++ BDSA-Assignment0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BDSA_Assignment0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
