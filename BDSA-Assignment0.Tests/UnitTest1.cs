42485470be500664ed09f883ba52ef841e3e20d3
Robin Ask Rohr Halberg <robin.hal6erg@gmail.com>
2021-09-03 16:15:05 +0200

    Added console-based user interface

--- BDSA-Assignment0.Tests/UnitTest1.cs
+++ BDSA-Assignment0.Tests/UnitTest1.cs
@@ -10,8 +10,7 @@ namespace BDSA_Assignment0.Tests
         [InlineData(12)]
         public void DivisibleByFour(int value)
         {
-            Program program = new Program();
-            var result = program.IsLeapYear(value);
+            var result = Program.IsLeapYear(value);
             Assert.True(result,$"{value} should be accepted as divisible by four");
         }
 
@@ -19,8 +18,7 @@ namespace BDSA_Assignment0.Tests
         [InlineData(3)]
         [InlineData(33)]
         public void NotDivisibleByFour(int value) {
-            Program program = new Program();
-            var result = program.IsLeapYear(value);
+            var result = Program.IsLeapYear(value);
             Assert.False(result,$"{value} should be denied as not divisible by four");
         }
 
@@ -28,8 +26,7 @@ namespace BDSA_Assignment0.Tests
         [InlineData(200)]
         [InlineData(500)]
         public void DivisibleByHundred(int value) {
-            Program program = new Program();
-            var result = program.IsLeapYear(value);
+            var result = Program.IsLeapYear(value);
             Assert.False(result,$"{value} should be denied as divisible by a hundred");
         }
 
@@ -38,8 +35,7 @@ namespace BDSA_Assignment0.Tests
         [InlineData(2000)]
         public void DivisibleByFourHundred(int value)
         {
-            Program program = new Program();
-            var result = program.IsLeapYear(value);
+            var result = Program.IsLeapYear(value);
             Assert.True(result,$"{value} should be accepted as divisible by four hundred");
         }
     }
--- BDSA-Assignment0/Program.cs
+++ BDSA-Assignment0/Program.cs
@@ -6,10 +6,23 @@ namespace BDSA_Assignment0
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Write 'exit' to end program");
+            var run = true;
+            while (run) {
+                Console.Write("Enter year: ");
+                var input = Console.ReadLine();
+                int year;
+                var parsed = int.TryParse(input, out year);
+                if (parsed) {
+                    if (IsLeapYear(year)) Console.WriteLine("yay");
+                    else Console.WriteLine("nay");
+                }
+                else if (input == "exit") run = false; 
+                else Console.WriteLine("Please enter an integer, or write 'exit' to end program");
+            }
         }
 
-        public bool IsLeapYear(int year) {
+        public static bool IsLeapYear(int year) {
             if(year % 4 != 0) return false;
             if (year % 400 == 0) return true;
             if (year % 100 == 0) return false;
