54293e3ca9321f2db800c2d501c5d87f7177baed
Robin Ask Rohr Halberg <robin.hal6erg@gmail.com>
2021-09-03 16:54:59 +0200

    Added error handling tests

--- BDSA-Assignment0/Program.cs
+++ BDSA-Assignment0/Program.cs
@@ -11,15 +11,22 @@ namespace BDSA_Assignment0
             while (run) {
                 Console.Write("Enter year: ");
                 var input = Console.ReadLine();
-                int year;
+                var answer = GetInputAnswer(input);
+                if (answer == "exit") run = false;
+                else Console.WriteLine(answer);
+            }
+        }
+
+        public static string GetInputAnswer(string input) {
+            int year;
                 var parsed = int.TryParse(input, out year);
                 if (parsed) {
-                    if (IsLeapYear(year)) Console.WriteLine("yay");
-                    else Console.WriteLine("nay");
+                    if (year < 1582) return "Year must be at least 1582";
+                    if (IsLeapYear(year)) return "yay";
+                    else return "nay";
                 }
-                else if (input == "exit") run = false; 
-                else Console.WriteLine("Please enter an integer, or write 'exit' to end program");
-            }
+                else if (input == "exit") return input; 
+                else return "Please enter an integer, or write 'exit' to end program";
         }
 
         public static bool IsLeapYear(int year) {
